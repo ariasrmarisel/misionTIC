4557c4637013000aaafa647896a39e00b393e675
ariasrmarisel <marisel_arias.r@hotmail.com>
2021-10-16 20:08:03 -0500

    Despliegue

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/AppContext.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/AppContext.cs
@@ -9,7 +9,7 @@ namespace ProyectoCiclo3.App.Persistencia
         public DbSet<Usuario> Usuarios { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
             if(!optionsBuilder.IsConfigured){
-                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ProyectoCiclo3");
+                optionsBuilder.UseSqlServer("Data Source = misiontic2022equipo0327.database.windows.net; Initial Catalog = ProyectoCiclo3; User ID=admin_equipo3_grupo27;Password=qWGmg8MWG9Q3qbK7");
             }
         } 
     }
